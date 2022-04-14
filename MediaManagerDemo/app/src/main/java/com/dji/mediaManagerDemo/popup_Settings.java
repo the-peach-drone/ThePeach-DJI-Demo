fc0680f607413b36508accbc3e33a34f31b20b01
WTPENGUIN <ccho2932@naver.com>
2022-04-14 11:38:17 +0900

    Change Setting activity to Popup Activity

--- MediaManagerDemo/app/src/main/java/com/dji/mediaManagerDemo/popup_Settings.java
+++ MediaManagerDemo/app/src/main/java/com/dji/mediaManagerDemo/popup_Settings.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 
 public class popup_Settings extends Activity{
@@ -17,6 +18,7 @@ public class popup_Settings extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.setting_connection);
         editTextIp = (EditText) findViewById(R.id.input_ip);
         editTextPort = (EditText) findViewById(R.id.input_port);
